72c150e52fe8a4c9609da509fe35f6d6501925dc
RatRug <135573411+RatRug@users.noreply.github.com>
2023-10-08 23:05:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,25 +15,6 @@ count=0
 pag.click()
 print("FF")
 while count<140:
-    """
-    image=pag.screenshot(region=(900,500,100,100)) 
-    try:
-        if pag.locate(Bubbles[0],image,confidence=.75) is not None or pag.locate(Bubbles[1],image, confidence=.75):
-            count+=1
-            print("Seen")
-            for x in 7:
-                pag.leftClick()
-                time.sleep(3.0/7)
-            pag.leftClick()
-            time.sleep(.5)
-        else:
-            print("Not Seen")
-            time.sleep(1)
-    except:
-        print("Error Switch")
-        time.sleep(2)
-    """
-    #try:
     for x in range(100):
             if pag.pixelMatchesColor(x+900,530,(68,252,234),tolerance=5):
                 count+=1
@@ -44,7 +25,6 @@ while count<140:
                 pag.leftClick()
                 time.sleep(.5)
                 break
-    #except:
     print("Not Seen "+ str(count))
     time.sleep(1)
     
